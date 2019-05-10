50e760f4c584ddffff36fa369ddaed77e0aaa337
JeffreyMinucci <jminucci2@gmail.com>
2019-05-10 15:36:24 -0400

    adjust test set to 0.2, add some models to compare to

--- gbm.R
+++ gbm.R
@@ -3,18 +3,18 @@ library(gbm)
 library(doParallel)
 
 df <- read.csv("data/missForest_Gambusia_Output.csv")
-drops <- c("STA_ID", "Date","X")
+drops <- c("STA_ID", "Date","X")  # keeping LONG and LAT in 
 df <- df[!is.na(df$THG_Fish),!(names(df) %in% drops)]
 df$ln_THG_Fish <- log(df$THG_Fish)
 df <- df[,names(df) != "THG_Fish"]
 
 # split to training and test datasets
 set.seed(1454)
-trainIndex <- createDataPartition(df$ln_THG_Fish, p=0.9, list = FALSE)
+trainIndex <- createDataPartition(df$ln_THG_Fish, p=0.8, list = FALSE)
 df_train <- df[trainIndex,]
 df_test <- df[-trainIndex,]
 
-fitControl <- trainControl(method='repeatedcv', number = 10, repeats = 10)
+fitControl <- trainControl(method='repeatedcv', number = 5, repeats = 5)
 
 # start parallel cluster
 cl <- makeCluster(10) # 10 cores
@@ -22,8 +22,8 @@ registerDoParallel(cl)
 
 ### GBM
 # define our grid of hyperparameters to try
-gbmGrid <- expand.grid(interaction.depth = c(1,3),
-                       n.trees = (1:75)*50,  # no cost to try a lot of these, as they are all derived from one run
+gbmGrid <- expand.grid(interaction.depth = c(1,3,5),
+                       n.trees = (1:75)*50,  # no cost to try a lot diff # of trees as they are all derived from one run
                        shrinkage = 0.01,
                        n.minobsinnode = c(10))
 
@@ -40,13 +40,13 @@ gbm1
 # end parallel cluster
 stopCluster(cl)
 
-# results and prediction accuracy
+# results and prediction accuracy -- best so far - Rsq = 0.62
 gbm_predict <- predict(gbm1, df_test, na.action = NULL)
 postResample(pred = gbm_predict, obs = df_test$ln_THG_Fish)
 plot(gbm1)
 
 varImp(gbm1)  # variable importance, scaled to 100 for most important
 
-plot(gbm1$finalModel, i.var = "Alk_Phos_SW", ntrees=100)
-plot(gbm1$finalModel, i.var = "MEHG_SW", ntrees=100)
+plot(gbm1$finalModel, i.var = "Alk_Phos_SW")
+plot(gbm1$finalModel, i.var = "MEHG_SW")
 
--- other_models.R
+++ other_models.R
@@ -1,11 +1,117 @@
 library(glmnet)
 library(caret)
 
-data <- read.csv('data/everglades_sem_cleaned.csv')[,-1]
 
-predictors <- model.matrix(ln_Hg_fish ~ ., data=data)
-response <- data[complete.cases(data), 'ln_Hg_fish']
-fit <- cv.glmnet(x = predictors, y = response)
-plot(fit)
-fit$lambda.min
-coef(fit,s='lambda.min')
+df <- read.csv("data/missForest_Gambusia_Output.csv")
+drops <- c("STA_ID", "Date","X")  # keeping LONG and LAT in 
+df <- df[!is.na(df$THG_Fish),!(names(df) %in% drops)]
+df$ln_THG_Fish <- log(df$THG_Fish)
+df <- df[,names(df) != "THG_Fish"]
+
+set.seed(1454)
+trainIndex <- createDataPartition(df$ln_THG_Fish, p=0.8, list = FALSE)
+df_train <- df[trainIndex,]
+df_test <- df[-trainIndex,]
+
+
+#fitControl <- trainControl(method='repeatedcv', number = 5, repeats = 5)
+fitControl <- trainControl(method='cv', number = 5)
+
+# start parallel cluster
+cl <- makeCluster(10) # 10 cores
+registerDoParallel(cl)
+
+#########################
+
+### random forest
+
+# best so far - Rsq = 0.62
+
+rf <- train(ln_THG_Fish ~ ., data = df_train,
+              method = "rf",
+              trControl = fitControl,
+              #preProcess = c("center", "scale"),
+              verbose = FALSE,
+              tuneLength = 20,  # try 20 different mtry values
+              na.action = na.pass)
+rf
+
+
+# end parallel cluster
+stopCluster(cl)
+
+# results and prediction accuracy
+rf_predict <- predict(rf, df_test, na.action = NULL)
+postResample(pred = rf_predict, obs = df_test$ln_THG_Fish)
+plot(rf) # show error vs mtry
+plot(rf$finalModel) # show error vs ntrees
+
+randomForest::varImpPlot(rf$finalModel)  # variable importance
+
+#########################
+
+### model-averaged neural networks
+
+# best so far - Rsq = 0.55
+
+cl <- makeCluster(10) # 10 cores
+registerDoParallel(cl)
+
+nnet_grid <- expand.grid(.decay = c(0.1, 1, 1.5, 3, 5), .size = c(2,3,4,5), .bag = c(F))
+
+nn <- train(ln_THG_Fish ~ ., data=df_train,
+            method = "avNNet",
+            trControl = fitControl,
+            tuneGrid = nnet_grid,
+            preProcess = c("center", "scale"),
+            linout=T,
+            repeats = 20,
+            verbose = FALSE)
+nn
+
+
+# end parallel cluster
+stopCluster(cl)
+
+# results and prediction accuracy
+nn_predict <- predict(nn, df_test, na.action = NULL)
+postResample(pred = nn_predict, obs = df_test$ln_THG_Fish)
+plot(nn)
+
+varImp(nn)  # variable importance, scaled to 100 for most important
+
+#########################
+
+### generalized linear model via penalized maximum likelihood (glmnet package)
+# alpha = 0 is the lasso penalty; alpha = 1 is the lasso penalty, alpha = 0 is the ridge penalty
+
+# best so far - Rsq = 0.55
+
+cl <- makeCluster(10) # 10 cores
+registerDoParallel(cl)
+
+glmnet_grid = expand.grid(alpha = 0:1, lambda = seq(0.0001, 1, length = 300))
+predictors <- model.matrix(ln_THG_Fish ~ .-1, data=df_train)
+glmr <- train(predictors, df_train$ln_THG_Fish,
+            method = "glmnet",
+            metric = "RMSE",
+            trControl = fitControl,
+            tuneGrid = glmnet_grid
+            #preProcess = c("center", "scale"),
+            )
+glmr
+
+
+# end parallel cluster
+stopCluster(cl)
+
+# results and prediction accuracy
+df_test_dummy =  model.matrix(ln_THG_Fish ~ .-1, data=df_test)
+glmr_predict <- predict(glmr, df_test_dummy, na.action = NULL)
+postResample(pred = glmr_predict, obs = df_test$ln_THG_Fish)
+plot(glmr)
+coef(glmr$finalModel, glmr$bestTune$lambda)
+
+
+
+
